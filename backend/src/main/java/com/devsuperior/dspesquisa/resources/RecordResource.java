f4f1eb4f08d8538da8f4a72626b81c45fa566281
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-10-17 11:28:53 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -4,9 +4,14 @@ import com.devsuperior.dspesquisa.dto.RecordDTO;
 import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
 import com.devsuperior.dspesquisa.services.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+
+import static org.springframework.data.domain.Sort.Direction;
 
 @RestController
 @RequestMapping(value = "/records")
@@ -19,6 +24,28 @@ public class RecordResource {
     public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
         RecordDTO newDTO = service.insert(dto);
         return ResponseEntity.ok().body(newDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<RecordDTO>> findAll(
+            @RequestParam(value = "min", defaultValue = "") String min,
+            @RequestParam(value = "max", defaultValue = "") String max,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+        Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+        Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+
+        if (linesPerPage == 0) {
+            linesPerPage = Integer.MAX_VALUE;
+        }
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        Page<RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+        return ResponseEntity.ok().body(list);
     }
 
 }
