e0b15400378d523186ae65d7cd80d92c858e98a2
Marta Denisczwicz Rozsa <marta.denisczwicz@gmail.com>
2021-10-18 14:03:09 -0300

    Add table

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Home from "./pages/Home"
-import Header from "./components/Header";
-import Records from "./pages/Records";
+import Header from "./components/Header"
+import Records from "./pages/Records"
 
 
 const Routes = () => (
